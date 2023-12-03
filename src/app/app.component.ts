3a5d1d479cfe3829a2d3f2faf45cf0323690be7c
wicked_ <bajno.damian96@gmail.com>
2023-12-03 17:15:23 +0100

    Display mocked posts app with bulma styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  posts = [
+    { title: 'Neat Tree', imageUrl: 'assets/tree.jpeg', username: 'nature', content: 'I saw this neat tree today' },
+    { title: 'Snowy Mountain', imageUrl: 'assets/mountain.jpeg', username: 'mountainlover', content: 'Here is a picture of a snowy mountain' },
+    { title: 'Mountain Biking', imageUrl: 'assets/biking.jpeg', username: 'biking12222', content: 'I did some biking today' }
+  ];
 }
