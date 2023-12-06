161deba938f035973ebd7e4b173fc09e9512566d
wicked_ <bajno.damian96@gmail.com>
2023-12-06 22:47:32 +0100

    Basic routing in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {ElementsModule} from "./elements/elements.module";
+import {CollectionsModule} from "./collections/collections.module";
 
-import { AppComponent } from './app.component';
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ElementsModule,
+    CollectionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
